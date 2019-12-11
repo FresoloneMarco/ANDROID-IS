ff912a10f6c5c5095fb14fe304d953def740bf4b
fiobig <fiobig@users.noreply.github.com>
2019-12-11 19:27:41 +0100

    Aggiunta delle pages visualizzazione e modifica profilo semi funzionanti

--- app/src/main/java/it/porting/android_is/gestioneStudente/MainActivityStudente.java
+++ app/src/main/java/it/porting/android_is/gestioneStudente/MainActivityStudente.java
@@ -3,12 +3,16 @@ package it.porting.android_is.gestioneStudente;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import it.porting.android_is.R;
+import it.porting.android_is.gestioneUtente.ViewActivityUtente;
 
 public class MainActivityStudente extends AppCompatActivity {
 
@@ -19,6 +23,19 @@ public class MainActivityStudente extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable (Color.rgb(255,153,0)));
         actionBar.setTitle("");
+        Button bmod;
+        bmod = findViewById(R.id.button2);
+        bmod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                modpage();
+            }
+        });
+    }
+
+    public void modpage(){
+        Intent intent = new Intent(getApplicationContext(), ViewActivityUtente.class);
+        startActivity(intent);
     }
 
     @Override
