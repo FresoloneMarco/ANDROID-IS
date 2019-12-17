19d4fb28c2db0b24f328caf366c632af51bffa5a
simovene <56631875+simovene@users.noreply.github.com>
2019-12-17 16:11:57 +0100

    Aggiunta impronta

--- app/src/main/java/it/porting/android_is/gestioneUtente/LoginActivity.java
+++ app/src/main/java/it/porting/android_is/gestioneUtente/LoginActivity.java
@@ -1,11 +1,16 @@
 package it.porting.android_is.gestioneUtente;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -14,6 +19,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,6 +31,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 import it.porting.android_is.R;
 import it.porting.android_is.gestioneAdmin.MainActivityAdmin;
@@ -40,6 +65,13 @@ public class LoginActivity extends AppCompatActivity {
     private String password;
     private Toast toast;
     private Boolean exit = false;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
+
+    private KeyStore keyStore;
+    private Cipher cipher;
+    private String KEY_NAME = "AndroidKey";
+
 
     // START FIRESTORE DECLARATION
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -50,17 +82,12 @@ public class LoginActivity extends AppCompatActivity {
     // [END declare_auth]
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-
-
         mAuth = FirebaseAuth.getInstance();
         bLogin = findViewById(R.id.bLogin);
         etEmail = findViewById(R.id.etEmail);
@@ -81,6 +108,105 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+            keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+            if (!fingerprintManager.isHardwareDetected()) {
+
+                Toast.makeText(getApplicationContext(), "Fingerprint Scanner not detected in Device", Toast.LENGTH_LONG);
+
+            } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(getApplicationContext(), "Permission not granted to use Fingerprint Scanner", Toast.LENGTH_LONG);
+
+
+            } else if (!keyguardManager.isKeyguardSecure()) {
+                Toast.makeText(getApplicationContext(), "Add Lock to your Phone in Settings", Toast.LENGTH_LONG);
+
+
+            } else if (!fingerprintManager.hasEnrolledFingerprints()) {
+
+                Toast.makeText(getApplicationContext(), "You should add atleast 1 Fingerprint to use this Feature", Toast.LENGTH_LONG);
+
+
+            } else {
+
+                Toast.makeText(getApplicationContext(), "Place your Finger on Scanner to Access the App.", Toast.LENGTH_LONG);
+
+
+                generateKey();
+
+                if (cipherInit()) {
+
+                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                    Fingerprint fingerprintHandler = new Fingerprint(this);
+                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
+
+                }
+            }
+
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generateKey() {
+
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+
+
+        } catch (KeyStoreException | IOException | CertificateException
+                | NoSuchAlgorithmException | InvalidAlgorithmParameterException
+                | NoSuchProviderException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+
+        try {
+
+            keyStore.load(null);
+
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            return true;
+
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
 
     }
 
