4c1cd7b7571f8b70aaeb7c6e1121d9a6b4daa5c5
simovene <56631875+simovene@users.noreply.github.com>
2019-12-21 20:34:52 +0100

    Modifica e aggiunta controlli in Register

--- app/src/main/java/it/porting/android_is/gestioneStudente/Register.java
+++ app/src/main/java/it/porting/android_is/gestioneStudente/Register.java
@@ -36,15 +36,15 @@ import it.porting.android_is.gestioneUtente.LoginActivity;
 
 public class Register extends AppCompatActivity {
 
-    private  EditText etNome;
-    private  EditText etCognome;
-    private  EditText etEmail;
-    private  EditText etPassword;
-    private  EditText etVPassword;
-    private  RadioGroup rdgroup_Sex;
+    private EditText etNome;
+    private EditText etCognome;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etVPassword;
+    private RadioGroup rdgroup_Sex;
     private ProgressBar progressBar;
-    private  Button btReg;
-    private  FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private Button btReg;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
@@ -55,17 +55,14 @@ public class Register extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-
-        etNome      = findViewById(R.id.etNome);
-        etCognome   = findViewById(R.id.etCognome);
-        etEmail     = findViewById(R.id.etEmail);
-        etPassword  = findViewById(R.id.etPassword);
+        etNome = findViewById(R.id.etNome);
+        etCognome = findViewById(R.id.etCognome);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
         etVPassword = findViewById(R.id.etV_password);
         rdgroup_Sex = findViewById(R.id.radio_Sex);
-        btReg       = findViewById(R.id.btReg);
+        btReg = findViewById(R.id.btReg);
         progressBar = findViewById(R.id.progressBar);
-
-
 
         btReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,187 +71,124 @@ public class Register extends AppCompatActivity {
             }
         });
 
-
-
     }
 
-    public void register(){
+    public void register() {
         progressBar.setVisibility(View.VISIBLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
-        final  String nome      = String.valueOf(etNome.getText());
-        final  String cognome   = String.valueOf(etCognome.getText());
-        final  String email     = String.valueOf(etEmail.getText());
-        final  String password  = String.valueOf(etPassword.getText());
-        final  String vpassword = String.valueOf(etVPassword.getText());
-        final  String sex =  "M";
-        final  Context context = this;
+        final String nome = String.valueOf(etNome.getText());
+        final String cognome = String.valueOf(etCognome.getText());
+        final String email = String.valueOf(etEmail.getText());
+        final String password = String.valueOf(etPassword.getText());
+        final String vpassword = String.valueOf(etVPassword.getText());
+        final String sex = "M";
+        final Context context = this;
 
 
 
-       if(nome.equals("") || cognome.equals("") || email.equals("") ||  password.equals("") || vpassword.equals("")){
+
+        //La registrazione non va a buon fine in quanto uno dei campi è vuoto
+        if (nome.equals("") || cognome.equals("") || email.equals("") || password.equals("") || vpassword.equals("")) {
             progressBar.setVisibility(View.GONE);
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
             Toast.makeText(this, "Non hai compilato tutti i campi", Toast.LENGTH_LONG).show();
-        }
 
-       else if (sex.length()>2) {
-
-           Toast.makeText(this, "Il campo non è stato compilato correttamente", Toast.LENGTH_SHORT).show();
-       }
-
-       else if(rdgroup_Sex.getCheckedRadioButtonId() == -1){
-           progressBar.setVisibility(View.GONE);
-           getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-           Toast.makeText(this, "Non hai inserito il sesso", Toast.LENGTH_LONG).show();
-       }
-
-       else if(password.length() <  8){
-           progressBar.setVisibility(View.GONE);
-           getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-
-           Toast.makeText(this, "La password non rispetta la lunghezza minima", Toast.LENGTH_LONG).show();
-
-       }
-
-        else if(!password.equals(vpassword)){
+        //La registrazione non va a buon fine in quanto il nome è in un formato non consentito
+        }else if((nome.matches(".*\\W+.*")|| (nome.matches(".*\\d+.*")))) {
             progressBar.setVisibility(View.GONE);
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(context, "Il campo Nome non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
 
-            Toast.makeText(this, "I campi password non corrispondono", Toast.LENGTH_LONG).show();
-        }
-        else {
+        //La registrazione non va a buon fine in quanto il cognome è in un formato non consentito
+        } else if((cognome.matches(".*\\W+.*")|| (nome.matches(".*\\d+.*")))) {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(context, "Il campo Nome non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
 
-                mAuth.createUserWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Map<String, Object> user = new HashMap<>();
-                                    user.put("nome", nome);
-                                    user.put("cognome", cognome);
-                                    user.put("email", email);
-                                    user.put("password", password);
-                                    user.put("sesso", sex);
-                                    user.put("ruolo", "studente");
-
-                                    db.collection("utenti").document(email)
-                                            .set(user, SetOptions.merge())
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void aVoid) {
-                                                    Log.d("DEBUG", "DocumentSnapshot successfully written!");
-                                                }
-                                            })
-                                            .addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    Log.w("DEBUG", "Error writing document", e);
-                                                }
-                                            });
-                                    progressBar.setVisibility(View.GONE);
-                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-
-                                    Toast.makeText(context, "Registrazione avvenuta con successo !", Toast.LENGTH_LONG).show();
-                                    Intent intent =  new Intent(getApplicationContext(), LoginActivity.class);
-                                    startActivity(intent);
-
-                                } else {
-                                    progressBar.setVisibility(View.GONE);
-                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-
-                                    Toast.makeText(context, "Errore durante la registrazione !", Toast.LENGTH_LONG).show();
-
-                                }
-
-
-                            }
-                        });
-
-            }
-
-
-        if(!nome.isEmpty()) {
-
-            for (int i = 0; i < nome.length(); i++) {
-
-                char ch = nome.charAt(i);
-
-                if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5'
-                        || ch == '6' || ch == '7' || ch == '8' || ch == '9') {
-
-                    progressBar.setVisibility(View.GONE);
-                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-
-                    Toast.makeText(context, "Errore durante la registrazione ! Il campo Nome non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }
-
-        else if(!cognome.isEmpty()) {
-
-            for (int i = 0; i < cognome.length(); i++) {
-
-                char ch = cognome.charAt(i);
-
-                if (ch == '0' || ch == '1' || ch == '2' || ch == '3' || ch == '4' || ch == '5'
-                        || ch == '6' || ch == '7' || ch == '8' || ch == '9') {
-
-                    progressBar.setVisibility(View.GONE);
-                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
-
-                    Toast.makeText(context, "Errore durante la registrazione ! Il campo Cognome non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        }
-
-        else if(!email.isEmpty()) {
-
-            String inziale = email.substring(0);
-
-
-            String punto = email.substring(1);
-            String centrale = email.substring(2, cognome.length());
-
-            if (!(email.endsWith("@studenti.unisa.it"))) {
-
+        //La registrazione non va a buon fine in quanto il prefisso dell'email non raggiunge la lunghezza minima consentita
+        }else if((email.substring(0, email.indexOf("@")).length() < 4)){
                 progressBar.setVisibility(View.GONE);
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                Toast.makeText(this, "Il campo email non rispetta la lunghezza minima consentita", Toast.LENGTH_LONG).show();
 
-                Toast.makeText(context, "Errore durante la registrazione ! Il campo email non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
+        //La registrazione non va a buon fine in quanto l'email non rispetta il formato consentito
+        }else if (!email.endsWith("@studenti.unisa.it") || !(email.substring(0, email.indexOf("@")).contains("."))) {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(this, "Il campo email non rispetta il formato consentito ", Toast.LENGTH_LONG).show();
 
+         //La registrazione non va a buon fine in quanto non è stato inserito il sesso
+        }else if (rdgroup_Sex.getCheckedRadioButtonId() == -1) {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(this, "Non hai inserito il sesso", Toast.LENGTH_LONG).show();
 
-            } else  if ((!inziale.equals(nome.charAt(0)))) {
+        //La registrazione non va a buon fine in quanto la password non raggiunge la lunghezza minima consentita
+        } else if (password.length() < 8) {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(this, "La password non rispetta la lunghezza minima consentita", Toast.LENGTH_LONG).show();
 
-                        progressBar.setVisibility(View.GONE);
-                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        //La registrazione non va a buon fine in quanto la password non rispetta il formato consentito
+        }else if(password.matches(".*\\W+.*")){
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(this, "La password non rispetta il formato consentito", Toast.LENGTH_LONG).show();
 
-                        Toast.makeText(context, "Errore durante la registrazione ! Il campo email non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
+        //La registrazione non va a buon fine in quanto le due password non coincidono
+        }else if (!password.equals(vpassword)) {
+            progressBar.setVisibility(View.GONE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            Toast.makeText(this, "I campi password non corrispondono", Toast.LENGTH_LONG).show();
 
-            } else if (((!punto.equals(".")))) {
+        } else {
 
-                        progressBar.setVisibility(View.GONE);
-                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("nome", nome);
+                                user.put("cognome", cognome);
+                                user.put("email", email);
+                                user.put("password", password);
+                                user.put("sesso", sex);
+                                user.put("ruolo", "studente");
 
-                        Toast.makeText(context, "Errore durante la registrazione ! Il campo email non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
+                                db.collection("utenti").document(email)
+                                        .set(user, SetOptions.merge())
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d("DEBUG", "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("DEBUG", "Error writing document", e);
+                                            }
+                                        });
+                                progressBar.setVisibility(View.GONE);
+                                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
-            } else if ((!centrale.equals(cognome))) {
+                                Toast.makeText(context, "Registrazione avvenuta con successo !", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
 
-                        progressBar.setVisibility(View.GONE);
-                        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                            } else {
+                                progressBar.setVisibility(View.GONE);
+                                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
 
-                        Toast.makeText(context, "Errore durante la registrazione ! Il campo email non rispetta il formato desiderato", Toast.LENGTH_LONG).show();
+                                Toast.makeText(context, "Email già esistente", Toast.LENGTH_LONG).show();
 
-            }
-
-
-
+                            }
+                        }
+                    });
         }
-
-
     }
 }
 
