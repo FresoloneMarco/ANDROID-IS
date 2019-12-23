c3c73b0d8651bd3d865df553011085b01d1507bd
simovene <56631875+simovene@users.noreply.github.com>
2019-12-23 20:37:10 +0100

    Aggiunta modifiche admin e segreteria

---
+++ app/src/main/java/it/porting/android_is/adapter/RequestAdapterAdmin.java
@@ -0,0 +1,138 @@
+package it.porting.android_is.adapter;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import it.porting.android_is.R;
+import it.porting.android_is.firebaseArchive.bean.RequestBean;
+
+
+/**
+ * Classe Adapter che consente la gestione della recyclerView nell'activity della segreteria
+ */
+public class RequestAdapterAdmin extends RecyclerView.Adapter <RequestAdapterAdmin.ViewHolder>{
+
+    ArrayList<RequestBean> arrayList;
+
+
+    public RequestAdapterAdmin(ArrayList<RequestBean> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+
+    /**
+     * Inserisce nella recyclerview la formattazione di ogni riga
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.request_item_admin, parent, false);
+
+        return new ViewHolder(v);
+
+
+    }
+
+
+    /**
+     * Inserisce in ogni campo il rispettivo valore
+     * @param holder la recyclerview
+     * @param position posizione della tupla
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        int formato = DateFormat.LONG;
+        DateFormat dateFormat = DateFormat.getDateInstance(formato, Locale.ITALY);
+        String dataRelease = dateFormat.format(arrayList.get(position).getRelease_date().toDate());
+        String dataExpiry = dateFormat.format(arrayList.get(position).getExpiry_date().toDate());
+        holder.idText.setText("ID richiesta: "  + Integer.toString(arrayList.get(position).getId()));
+        holder.livelloText.setText("Livello: " +  arrayList.get(position).getLevel());
+        holder.releaseText.setText("Rilascio: " + dataRelease);
+        holder.expiryText.setText("Scadenza: " + dataExpiry);
+        holder.annoText.setText("Anno: " + arrayList.get(position).getYear());
+        holder.serialeText.setText("Seriale: " + Integer.toString(arrayList.get(position).getSerial()));
+        holder.cfuText.setText("CFU: " + Integer.toString(arrayList.get(position).getValidated_cfu()));
+        holder.utenteText.setText("Studente: " + arrayList.get(position).getUser_name() + " " + arrayList.get(position).getUser_surname());
+        holder.emailText.setText("Email: " + arrayList.get(position).getUser_key());
+        holder.enteText.setText("Ente :" + arrayList.get(position).getEnte());
+        holder.statoText.setText("Stato :" + arrayList.get(position).isStato());
+
+
+    }
+
+
+    /**
+     * conteggio elementi
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+
+    /**
+     * classe "gestore" della recyclerview, in cui vengono inizializzati tutti i campi.
+     */
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        View root;
+        TextView idText;
+        TextView utenteText;
+        TextView emailText;
+        TextView livelloText;
+        TextView releaseText;
+        TextView expiryText;
+        TextView annoText;
+        TextView serialeText;
+        TextView cfuText;
+        TextView enteText;
+        TextView statoText;
+
+
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView;
+            idText = root.findViewById(R.id.idText);
+            utenteText = root.findViewById(R.id.utenteText);
+            emailText = root.findViewById(R.id.emailText);
+            livelloText = root.findViewById(R.id.livelloText);
+            releaseText = root.findViewById(R.id.releaseText);
+            expiryText = root.findViewById(R.id.expiryText);
+            annoText = root.findViewById(R.id.annoText);
+            serialeText = root.findViewById(R.id.serialeText);
+            cfuText = root.findViewById(R.id.cfuText);
+            enteText = root.findViewById(R.id.enteText);
+            statoText = root.findViewById(R.id.statoText);
+
+
+
+        }
+    }
+
+
+
+
+
+}
