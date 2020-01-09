fd21cd1c801d71432453072070fc7c8959399cc9
Marco <m.fresolone2@studenti.unisa.it>
2020-01-09 10:15:19 +0100

    import di downloadManager

--- app/src/main/java/it/porting/android_is/gestioneAdmin/MainActivityAdmin.java
+++ app/src/main/java/it/porting/android_is/gestioneAdmin/MainActivityAdmin.java
@@ -1,5 +1,6 @@
 package it.porting.android_is.gestioneAdmin;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
